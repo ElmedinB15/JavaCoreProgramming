ffdad6498e800ec5ba1be9c902e813b8a6931bfb
Elmedin <>
2019-12-27 11:36:26 +0100

    Pomijeranje zvjezdice desno-lijevo do beskonačnosti

---
+++ src/Five/CallCalculator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Five;
+
+
+public class CallCalculator {
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+        c.ADD(31,13);
+         
+        System.out.println(c.add());
+        System.out.println(c.div());
+        System.out.println(c.sub());
+        System.out.println(c.mul());
+    }
+    
+}
