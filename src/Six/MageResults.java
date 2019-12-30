143581f1390ddd2718b3086730e452f30bf1878b
Elmedin <>
2019-12-30 11:23:35 +0100

    Testing ElfMage class

---
+++ src/Six/MageResults.java
@@ -0,0 +1,14 @@
+
+package Six;
+
+public class MageResults {
+
+    public static void main(String[] args) {
+        
+        ElfMage call = new ElfMage(100,30,80,90);
+        
+       call.Go();
+      
+    }
+    
+}
