0600071619488b7991563f12ff78019cfd7ad56e
Elmedin <>
2020-01-08 00:07:47 +0100

    -Kreiranje klase za izbacivanje rezultata kroz vremenski period -Koristeci Timer i Thread

---
+++ src/Seven/RunThread.java
@@ -0,0 +1,20 @@
+
+package Seven;
+
+
+
+ public class RunThread extends Thread {
+
+   public void run(){
+       
+       for(int i =0;i<5;i++){
+           
+           System.out.println("Good morning!");
+         try{Thread.sleep(1000);} catch (Exception e){}
+}
+    
+}
+   
+}
+
+    
---
+++ src/Seven/Thread_use.java
@@ -0,0 +1,18 @@
+
+package Seven;
+
+
+public class Thread_use {
+
+    public static void main(String[] args) {
+        
+        RunThread runT = new RunThread();
+     
+        
+        runT.start();
+       
+        
+        
+        
+    }
+}
